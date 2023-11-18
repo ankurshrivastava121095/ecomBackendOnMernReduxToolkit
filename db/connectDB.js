5d841cca424cbff3ebedf6d1ac6893a65b16986e
pninfo346 <pninfo643@gmail.com>
2023-11-18 15:08:19 +0530

    live db

--- db/connectDB.js
+++ db/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 
 const connectDB = () => {
-    return mongoose.connect(process.env.DB_LOCAL)
+    return mongoose.connect(process.env.DB_LIVE)
     .then((data)=>{
         console.log(`MongoDB connected with server: ${data.connection.host}`);
     }).catch((err)=>{
